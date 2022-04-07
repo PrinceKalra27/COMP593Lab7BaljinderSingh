8c585bec474b9fa8ca41f324acf84bc60d15f667
Baljinder Singh <98180347+PrinceKalra27@users.noreply.github.com>
2022-04-07 08:11:39 -0400

    Create Lab7BaljinderSingh.py

---
+++ Lab7BaljinderSingh.py
@@ -0,0 +1,13 @@
+#Lab7
+def main():
+    #initilizing
+    'name' : 'Baljinder Singh' ,
+    'stdnt_id' : '10266917' ,
+    'topings' : [
+        'corns',
+        'chicken'
+        'paneer'
+    ]
+    'films' : [
+        
+    ]
